42bb0c00f53cce9da46e3be32746c2b448a74b1c
svytoslav <svytoslavother@gmail.com>
2025-03-21 21:06:49 +0200

    refactor/app.jsx
    - fix app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import logo from './img/logo.png'
+
+function App() {
+
+  return (
+    <>
+      <div className="Homepage">
+        <img src={logo} alt='logo' />
+      </div>
+    </>
+  )
+}
+
+export default App
